316883ddd018379e373aac8fcd5cf46fd720994b
Ken Ahrens <ken.ahrens@gmail.com>
2016-08-09 22:42:40 -0400

    upload completed plugin script

--- README.md
+++ README.md
@@ -3,11 +3,62 @@
 
 This plugin runs queries against New Relic Insights to calculate things like % of locations with a failure.
 
+### Installation and Setup
+Here are the steps to get set up with this plugin:
+* Pre-requisite is NodeJS (4.x or newer) and npm
+* Clone or download this repository
+* Once in the directory run ```npm install```
+* Setup your configuration either for single account or multi account
+
+Once you are all set up you can run the plugin with ```npm start```
+
 ### Multi Account: Configuration JSON
-If you want to query metrics from multiple accounts and post to a specific result account, you must setup a JSON config file of your own.
+If you want to query metrics from multiple accounts and post to a specific result account, you must setup a JSON config file of your own. Then before you run the plugin you want to set an environment variable ```NODE_ENV``` with a value of the name of your configuration. Here is an example of how to setup a custom JSON file with multiple sets of keys. At runtime you must set NODE_ENV to the name of this config. (Note that if you want to change the GUID you can put a new value in your custom config and it will over-ride the value in default.json.)
+```
+{
+  "guid": "com.newrelic.fit.synthetics.monitor.Synthetics2",
+  "duration": 30,
+  "licenseKey": "",
+  "MasterAccount": {
+    "accountId": "",
+    "insightsQueryKey": ""
+  },
+  "SubAccount1": {
+    "accountId": "",
+    "insightsQueryKey": ""
+  },
+  "SubAccount2": {
+    "accountId": "",
+    "insightsQueryKey": ""
+  }
+}
+```
 
 ### Single Account: Environment Variables
 If you want to query metrics from a single account and post to the same account, you can just set these 3 environment variables:
 * NEWRELIC_LICENSE_KEY maps to licenseKey (for plugin to publish metrics)
 * NEWRELIC_ACCOUNT_ID maps to accountId
 * NEWRELIC_INSIGHTS_QUERY_KEY maps to insightsQueryKey (for plugin to query metrics)
+
+### Running the Plugin
+
+### Running the Tests
+The tests work if you're using a single account and will make sure your node environment is setup properly. All you have to do to run them is:
+```
+kahrens:nr-synthetics-plugin kahrens$ npm test
+
+> nr-synthetics-plugin@1.0.0 test /Users/kahrens/Documents/github/nr-synthetics-plugin
+> mocha
+
+
+
+  New Relic Insights API Test
+    ✓ calls the query api (271ms)
+
+  New Relic Plugin Make Metric Test
+    ✓ makes a full metric name
+    ✓ makes the complete metric JSON
+
+
+  3 passing (283ms)
+```
--- index.js
+++ index.js
@@ -1,9 +1,84 @@
-const client = require('newrelic-api-client');
-const insights = client.insights;
-const CronJob = require('cron').CronJob;
+const insights = require('./lib/insights.js');
+const plugins = require('./lib/plugins.js');
 
-var poll = function() {
-  console.log('Poll');
+const CronJob = require('cron').CronJob;
+const winston = require('winston');
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+      timestamp: function() {return new Date().toUTCString();}
+    })
+  ]
+})
+
+// Configuration used for Insights queries
+var configId = 'newrelic';
+var monitorListNQRL = "SELECT uniques(monitorName) FROM SyntheticCheck";
+var locationStatusNRQL = "SELECT latest(result) FROM SyntheticCheck FACET locationLabel WHERE monitorName = '{monitorName}'"
+
+// Publish the metric to the Plugin API
+var reportMetric = function(monitorName, successRate, configId) {
+  logger.info('Report Metric: ' + monitorName + ' (' + successRate + ')');
+  var successMetricName = plugins.makeMetricName(monitorName, 'Location Success', 'pct');
+  var failMetricName = plugins.makeMetricName(monitorName, 'Location Fail', 'pct');
+  var metricArr = {};
+  metricArr[successMetricName] = successRate;
+  metricArr[failMetricName] = 100 - successRate;
+  plugins.post(metricArr, configId, function(error, response, body) {
+    if (response.statusCode == 200) {
+      logger.info(body);
+    } else {
+      logger.error('Response to Plugin POST: ' + response.statusCode);
+      logger.error(body);
+    }
+  });
 }
 
-var job = new CronJob('* * * * * *', poll());
+// Get the location status for the given monitor
+var getLocationStatus = function(monitorName) {
+  logger.info('Get status for: ' + monitorName);
+  var nrql = locationStatusNRQL.replace('{monitorName}', monitorName);
+  insights.query(nrql, configId, function(error, response, body) {
+    if (response.statusCode == 200) {
+      var facets = body.facets;
+      var success = 0;
+      for (var i = 0; i < facets.length; i++) {
+        var latest = facets[i].results[0].latest;
+        if (latest == 'SUCCESS') {
+          success++;
+        }
+      }
+      var successRate = 100 * success / facets.length;
+      reportMetric(monitorName, successRate, configId);
+    } else {
+      logger.error('Response to Insights location status: ' + response.statusCode);
+      logger.error(body);
+    }
+  });
+}
+
+// Get the list of monitors
+var getMonitorList = function() {
+  var nrql = monitorListNQRL;
+  insights.query(nrql, configId, function(error, response, body) {
+    if (response.statusCode == 200) {
+      var monitors = body.results[0].members;
+      for (var i = 0; i < monitors.length; i++) {
+        var monitorName = monitors[i];
+        getLocationStatus(monitorName);
+      }
+    } else {
+      logger.error('Response to Insights monitor list: ' + response.statusCode);
+      logger.error(body);
+    }
+  });
+}
+
+// Run every {duration} seconds
+var cronTime = '*/30 * * * * *';
+var job = new CronJob(cronTime, function() {
+  logger.info('Starting poll');
+  getMonitorList();
+});
+job.start();
+logger.info('Job started');
