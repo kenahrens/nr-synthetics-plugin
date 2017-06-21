d5589518ea6e3ce518e2967804dd9a45d93f40aa
Ken Ahrens <kahrens@newrelic.com>
2017-06-21 15:16:00 -0400

    updating version 2.1.0 which makes fewer queries to Insights

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const insights = require('./lib/insights.js');
-const plugins = require('./lib/plugins.js');
 const config = require('config');
 const CronJob = require('cron').CronJob;
-const winston = require('winston');
 const version = require('./package.json').version;
 
-var logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
-      timestamp: function() {return new Date().toUTCString();},
-      level: 'info'
-    })
-  ]
-})
+// These are helper libraries
+const helper = require('./lib/helper.js');
+const insights = require('./lib/insights.js');
+const byMonitor = require('./lib/byMonitor.js');
+const byLocation = require('./lib/byLocation.js');
 
 // Insights queries
-var monitorListNQRL = "SELECT uniques(monitorName) FROM SyntheticCheck";
-var locationStatusNRQL = "SELECT latest(result), latest(duration) FROM SyntheticCheck FACET locationLabel WHERE monitorName = '{monitorName}' SINCE 15 minutes ago LIMIT 100"
+var uniqueCountNRQL = "SELECT uniques(monitorName), uniques(locationLabel) FROM SyntheticCheck";
 
 // Global variables
 var freq = config.get('duration');
@@ -25,146 +18,26 @@ var guid = config.get('guid');
 var cronTime = '*/' + freq + ' * * * * *';
 var single = true;
 
-// This will report the data from the Metric Array to Insights
-var reportEvent = function(monitorName, insightsMetricArr, configId) {
-  var event = {};
-  event.eventType = 'ExtraSyntheticsInfo';
-  event.timestamp = new Date().getTime();
-  event.monitorName = monitorName;
-  for (var attribute in insightsMetricArr) {
-    event[attribute] = insightsMetricArr[attribute];
-  }
-  
-  insights.publish(event, configId, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      logger.debug('Posted Insights event for ' + monitorName);
-    } else {
-      if (error) {
-        logger.error('Error in Insights POST');
-        logger.error(error);
-      } else {
-        logger.error('Response to Insights POST: ' + response.statusCode);
-        logger.error(body);
-      }
-    }
-  });
-}
-
-// This will report the data from the Metric Array to Plugins
-var reportMetric = function(monitorName, pluginMetricArr, configId) {
-  plugins.post(monitorName, pluginMetricArr, configId, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      logger.debug('Posted ' + Object.keys(pluginMetricArr).length + ' metrics to ' + monitorName);
-    } else {
-      if (error) {
-        logger.error('Error in Plugin POST');
-        logger.error(error);
-      } else {
-        logger.error('Response to Plugin POST: ' + response.statusCode);
-        logger.error(body);
-      }
-    }
-  });
-}
-
-// Helper function reads the latest result and duration and calculates all metrics
-// Result % (success / fail) and Duration per location
-// Overall Result % (success / fail) and Duration
-var calculateMetrics = function(monitorName, facets, configId) {
-  var successCount = 0;
-  var sumDuration = 0;
-  var pluginMetricArr = {};
-  var insightsMetricArr = {};
-  
-  for (var i = 0; i < facets.length; i++) {
-    var locationName = 'Location/' + facets[i].name;
-    var locResult = facets[i].results[0].latest;
-    var locDuration = facets[i].results[1].latest;
-    
-    // Create the metric names for this location
-    var metricSuccessPct = plugins.makeMetricName(locationName, 'Success', 'pct');
-    var metricFailPct = plugins.makeMetricName(locationName, 'Failure', 'pct');
-    var metricDuration = plugins.makeMetricName(locationName, 'Duration', 'ms');
-    pluginMetricArr[metricDuration] = locDuration;
-
-    sumDuration += locDuration;
-    if (locResult == 'SUCCESS') {
-      successCount++;
-      pluginMetricArr[metricSuccessPct] = 100;
-      pluginMetricArr[metricFailPct] = 0;
-    } else {
-      pluginMetricArr[metricSuccessPct] = 0;
-      pluginMetricArr[metricFailPct] = 100;
-    }
-  }
-
-  var successRate = 100 * successCount / facets.length;
-  var avgDuration = sumDuration / facets.length;
-
-  // Create the rollup metric names
-  var metricRollupSuccessCount = plugins.makeMetricName('Overall', 'Success', 'count'); 
-  var metricRollupSuccessPct = plugins.makeMetricName('Overall', 'Success', 'pct');
-  var metricRollupFailCount = plugins.makeMetricName('Overall', 'Failure', 'count'); 
-  var metricRollupFailPct = plugins.makeMetricName('Overall', 'Failure', 'pct');
-  var metricRollupDuration = plugins.makeMetricName('Overall', 'Duration', 'ms');
-  
-  // Store the values in the plugin metric array
-  pluginMetricArr[metricRollupSuccessCount] = successCount;
-  pluginMetricArr[metricRollupSuccessPct] = successRate;
-  pluginMetricArr[metricRollupFailCount] = facets.length - successCount;
-  pluginMetricArr[metricRollupFailPct] = 100 - successRate;
-  pluginMetricArr[metricRollupDuration] = avgDuration;
-
-  // Store the values in the insights metric array
-  insightsMetricArr['successCount'] = successCount;
-  insightsMetricArr['successRate'] = successRate;
-  insightsMetricArr['failCount'] = facets.length - successCount;
-  insightsMetricArr['failRate'] = 100 - successRate;
-  insightsMetricArr['locationCount'] = facets.length;
-  insightsMetricArr['duration'] = avgDuration;
-  
-  reportMetric(monitorName, pluginMetricArr, configId);
-  reportEvent(monitorName, insightsMetricArr, configId);
-}
-
-// Get the location status and duration for the given monitor
-var getLocationStatus = function(monitorName, configId) {
-  logger.debug('getLocationStatus for: ' + monitorName);
-  var escapedName = monitorName.replace('\'', '\\\'');
-  var nrql = locationStatusNRQL.replace('{monitorName}', escapedName);
+// Get the count of monitors and locations
+var getUniqueCounts = function(configId) {
+  logger.debug('getUniqueCounts for config: ' + configId);
+  var nrql = uniqueCountNRQL;
   insights.query(nrql, configId, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-      calculateMetrics(monitorName, body.facets, configId);
-    } else {
-      if (error) {
-        logger.error('Error on Insights location status call');
-        logger.error(error);
-      } else {
-        logger.error('Response to Insights location status: ' + response.statusCode);
-        logger.error(body);
-      }
-    }
-  });
-}
-
-// Get the list of monitors
-var getMonitorList = function(configId) {
-  logger.info('getMonitorList for config: ' + configId);
-  var nrql = monitorListNQRL;
-  insights.query(nrql, configId, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
+      
+      // Pull monitors and locations from the response
       var monitors = body.results[0].members;
-      for (var i = 0; i < monitors.length; i++) {
-        var monitorName = monitors[i];
-        getLocationStatus(monitorName, configId);
-      }
-    } else {
-      if (error) {
-        logger.error('Error on Insights monitor list call');
-        logger.error(error);
+      var monitorCount = monitors.length;
+      var locations = body.results[1].members;
+      var locationCount = locations.length;
+      
+      // Determine which approach will call Insights the least (# of locations or # of monitors)
+      if (monitorCount > locationCount) {
+        logger.info('- Query by location: (' + locationCount + ') locations < (' + monitorCount + ') monitors');
+        byLocation.start(configId, locations);
       } else {
-        logger.error('Response to Insights monitor list: ' + response.statusCode);
-        logger.error(body);
+        logger.info('- Query by monitor: (' + monitorCount + ') monitors < (' + locationCount + ') locations');
+        byMonitor.start(configId, monitors);
       }
     }
   });
@@ -181,7 +54,8 @@ var job = new CronJob(cronTime, function() {
   // Loop through each of the configurations 
   for (var i = 0; i < configArr.length; i++) {
     var configId = configArr[i];
-    getMonitorList(configId);
+    // getMonitorList(configId);
+    getUniqueCounts(configId);
   }
 });
 
@@ -193,7 +67,7 @@ if (configArr.length == 1) {
   logger.info('* Running as a single config.');
   single = true;
 } else {
-  logger.info('* Running as a multi config.');
+  logger.error('* Running as a multi config is not implemented');
   single = false;
 }
 job.start();
